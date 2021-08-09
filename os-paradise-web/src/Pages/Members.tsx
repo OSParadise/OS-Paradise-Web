0c8cd7c08e791f51986bee47f194be0888ebb6ea
Brad Whitesell <whitesellbt@gmail.com>
2021-08-09 00:35:10 -0700

    Added basic rendering for members

--- os-paradise-web/src/Pages/Members.tsx
+++ os-paradise-web/src/Pages/Members.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Card, CardFooter, CardText, CardTitle, Row } from "reactstrap";
 import { GetGroupMembers } from "../API/WiseOldManApi";
 import { IPlayer } from "../Interfaces";
 
@@ -25,7 +26,27 @@ function Members() {
         paddingTop: "40px",
         textAlign: "left",
       }}
-    ></div>
+    >
+      <legend style={{ textAlign: "left", color: "#FFF", paddingTop: "20px" }}>
+        Members{" "}
+        <span role="img" aria-label="women holding hands">
+          👭
+        </span>
+      </legend>
+      {players &&
+        players.map((value) => (
+          <Row key={value.id}>
+            <Card body inverse style={{ backgroundColor: "#40444b", borderColor: "#333", maxWidth: "600px" }}>
+              <CardTitle tag="h5" style={{ color: "#FFF" }}>
+                {value.displayName}
+              </CardTitle>
+              <CardText>{value.role}</CardText>
+              <CardText>{value.build}</CardText>
+              <CardFooter>{value.joinedAt}</CardFooter>
+            </Card>
+          </Row>
+        ))}
+    </div>
   );
 }
 
